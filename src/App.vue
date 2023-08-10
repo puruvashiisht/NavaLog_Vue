3f85a9d0d3a70d5c5e5744d443064240f25d37c4
Puru Vashiisht <81155092+puruvashiisht@users.noreply.github.com>
2023-08-09 19:57:21 -0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   
   
- 
+  <div class="logout">
+       <router-link to ="/" @click="logout">Logout</router-link>
+        </div>
  
   <div class="container">
     <img alt="NavaLog" src="./assets/NavaLog.png" class="logo">
@@ -10,18 +12,22 @@
     
     </div> 
 
-    <div>
+    <div class="vessel">
       <h1>Vessel Finder</h1>
-      <img alt="map" src="./assets/vessellfinder.png" class="logo">
+      
+      <img alt="map" src="./assets/vessellfinder.png" class="VesselFinder">
+     
+    
     </div>
    
     
 <div class="footer">
   <h3>Meet the creators</h3>
   
-    <img alt="Logo" src="./assets/creators.png" class="rounded" style="overflow-x:auto;">
+    <img alt="Logo" src="./assets/creators.png" class="logo" style="overflow-x:auto;">
 </div>
     
+
  
   
 </template>
